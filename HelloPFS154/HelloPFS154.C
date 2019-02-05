0122a2390c6f11006067bb815d4af6e00c1a6d10
free-pdk <free-pdk@users.noreply.github.com>
2019-02-05 17:42:38 +0100

    change to 9600 baud, fix shift direction, optimizations

--- HelloPFS154/HelloPFS154.C
+++ HelloPFS154/HelloPFS154.C
@@ -1,9 +1,9 @@
 // "Hello, world!" for the Padauk PFS154, ported to PADAUK mini-C.
 // Written by Philipp Klaus Krause in 2019.
-// Ported by free-pdk
+// Ported and adapted by free-pdk
 // Source code under CC0 1.0.
 
-// Output on PA7 at 1200 baud.
+// Output on PA7 at 9600 baud.
 
 
 BYTE sendcounter;
@@ -15,20 +15,13 @@ void Interrupt(void)
   pushaf;                   // save registers
   if(intrq.TM2)             // only handle TM2 interrupts
   {
-    intrq.TM2 = 0;
-    if(sendcounter)         // only send bits if required
-	{
-      A = (senddata$1) & 2; // test bit 9 of senddata (senddata$0:senddata$1 = 8,7,6,5,4,3,2,1 : 16,15,14,13,12,10,9 => 0x00:0x02 => 0x200)
-      if( A )
-        PA.7 = 1;           // set PA.7 accordingly
-      else
-        PA.7 = 0;
+	SR senddata$1           // shift right upper part of senddata
+	SRC senddata$0          // shift right with carry lower part of senddata 
+	SWAPC PA.7              // use carry from shit to set bit on IO
+	if( !--sendcounter )    // all bits sent?
+	  inten.TM2 = 0;        // disable TM2 interrupt
 
-      senddata <<= 1;       // shift left senddata to get next bit in position
-  
-      if(!--sendcounter)    // all bits sent?
-        inten.TM2 = 0;      // disable TM2
-	}
+    intrq.TM2 = 0;          // mark this interrupt event handled
   }
   popaf;                    // restore registers
 }
@@ -40,10 +33,10 @@ void _putchar(void)
   //while(sending){};
   while(inten.TM2){};
 
-  senddata = (A << 1) | 1;  //construct 1 start bit (0), 8 data bits, 1 stop bit (1)
+  senddata = (A << 1) | 0x200;  // construct 1 start bit (0), 8 data bits, 1 stop bit (1)
   sendcounter = 10;
-  tm2ct = 0;
-  inten.TM2 = 1;
+  tm2ct = 0;                    // reset TM2 counter
+  inten = 0x40;                 // enable TM2 interrupt
 }
 
 //unsigned char _sdcc_external_startup(void)
@@ -54,16 +47,15 @@ void _sdcc_external_startup(void)
 
 void main(void)
 {
-  tm2c = 0x10;   // Use SYSCLK (8 Mhz), Period
-  tm2s = 0x60;   // Prescaler 64 , Scaler 1, timer always divedes by 2 ~> 62.5 kHz
-  tm2b = 60;     // Divide by 52 ~> 1201 Hz //60 by oscilloscope on PFS154, 118 by oscilloscope on ICE
-  inten = 0x00;  // disable all interrupts
-
-  engint;        // global interrupt enable
+  pac = 0x80;      // enable PA.7 as output for serial
+  tm2c = 0x20;     // enable TM2 (IHRC 16 Mhz, Period)
+  tm2s = 0x20;     // pre scaler 4, scaler 1, timer always divides by 2 > 2MHz 
+  tm2b = 208;      // divide by 208 > 9615 Hz
+  inten = 0;       // no interrupt selected
+  engint;          // global interrupt enable
     
-  pac = 0x80;    // enable PA.7 as output
-
-  putchar(0x55); // send initial char for autobaud
+  putchar(0x55);   // send initial char for autobaud
+  .delay 8000      // wait 1 ms for autobaud to complete
   
   while(1)
   {
